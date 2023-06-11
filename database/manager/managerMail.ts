1b1d3d4f2049d9d2ebb667870c05fbb898fff7ca
stepo <StefanoPutelli>
2023-06-11 20:59:37 +0200

    modifica get env var

--- database/manager/managerMail.ts
+++ database/manager/managerMail.ts
@@ -1,16 +1,16 @@
 import emailjs from '@emailjs/nodejs';
-import * as dotenv from 'dotenv'
+// import * as dotenv from 'dotenv'
 
 export function emailValida(email: string): boolean {
     var regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     return regexp.test(email);
 }
 
-const envs = dotenv.config()
+// const envs = dotenv.config()
 
 emailjs.init({
-    publicKey: envs.parsed.EMAILJS_PUBLIC,
-    privateKey: envs.parsed.EMAILJS_PRIVATE, // optional, highly recommended for security reasons
+    publicKey: process.env.EMAILJS_PUBLIC,
+    privateKey: process.env.EMAILJS_PRIVATE, // optional, highly recommended for security reasons
 });
 
 
